90a275776a3684d39e34762d8784d9d486548fac
g-martelly <nxt.tech@gmail.com>
2020-11-26 16:18:44 -0800

    Created basic HTML and JS

---
+++ script.js
@@ -0,0 +1,6 @@
+var dateField = document.getElementById('date');
+var timeField = document.getElementById('time');
+
+console.log(dateField);
+dateField.innerText = "Date from JS";
+timeField.innerText = "Time from JS";
