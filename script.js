0e8adfd751fdf2acfce1d555766ce5cac434c9d9
g-martelly <nxt.tech@gmail.com>
2020-11-26 17:47:37 -0800

    Date and Time and are working.

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 var dateField = document.getElementById('date');
 var timeField = document.getElementById('time');
+var date = new Date();
+var daysOfTheWeek = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+var months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+var currentDay= daysOfTheWeek[date.getDay()];
+var month = months[date.getMonth()]; 
+var year = date.getFullYear();
+var day = date.getDay();
+
+var hour = date.getHours();
+var minute = date.getMinutes();
+var seconds = date.getSeconds();
+
 
 console.log(dateField);
-dateField.innerText = "Date from JS";
-timeField.innerText = "Time from JS";
+dateField.innerText = currentDay + ", " + month + " " + day + ", " + year;
+timeField.innerText = hour + ":" + minute + ":" + seconds;
