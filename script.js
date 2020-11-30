030b1edb40e6c831d6fe1d07ba55a1566691623c
gmartelly <11716024+g-martelly@users.noreply.github.com>
2020-11-29 23:38:16 -0800

    Develop (#1)
    * Date and Time and are working.
    
    * Added 12/24hr toggle button
    
    * Fixed toggle button bug and updated button label
    
    * Fixed day bug and added CSS

--- script.js
+++ script.js
@@ -1,6 +1,76 @@
-var dateField = document.getElementById('date');
-var timeField = document.getElementById('time');
 
-console.log(dateField);
-dateField.innerText = "Date from JS";
-timeField.innerText = "Time from JS";
+const timer = setInterval(displayDateAndTime, 1000);
+var toggle = 24;
+document.getElementById('12hourFormat').hidden = true;
+
+function displayDateAndTime() {
+    //Get date
+    const date = new Date();
+    var daysOfTheWeek = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    var months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+    var currentDay= daysOfTheWeek[date.getDay()];
+    var month = months[date.getMonth()]; 
+    var year = date.getFullYear();
+    var day = date.getDate();
+
+   // console.log("day is + " + day + " " + date);
+
+    //Get time
+    var hour = date.getHours();
+    var minute = date.getMinutes();
+    var seconds = date.getSeconds();
+    
+    //Add leading zeros for the time
+    hour = ("0" + hour).slice(-2);
+    minute = ("0" + minute).slice(-2);
+    seconds = ("0" + seconds).slice(-2);
+
+    //Grab and update the HTML elements
+    var dateField = document.getElementById('date');
+    var timeField24h= document.getElementById('24hourFormat');
+    var timeField12h= document.getElementById('12hourFormat');
+
+    dateField.innerHTML = currentDay + ", " + month + " " + day + "," + year;
+    timeField24h.innerHTML = hour + ":" + minute + ":" + seconds;
+    timeField12h.innerHTML = calculateHour() + ":" + minute + ":" + seconds + " " + am_pm();
+
+    function am_pm () {
+        if (hour > 11) {
+            return "PM";
+        } else {
+            return "AM";
+        } 
+    }
+
+    function calculateHour(){
+        var h = date.getHours()
+        if (h == 0) {
+            return 12;
+        } 
+         if (hour > 12) {
+            return h - 12;
+        } else {
+            return hour;
+        }
+
+    }
+}
+
+document.getElementById("toggleButton").onclick = function() {toggleFormat()};
+
+function toggleFormat() {
+    if (toggle === 24){
+        document.getElementById('24hourFormat').hidden = true;
+        document.getElementById('12hourFormat').hidden = false;
+        document.getElementById("toggleButton").innerHTML = "24 Hour";
+        toggle = 12;
+
+    } else {
+        document.getElementById('24hourFormat').hidden = false;
+        document.getElementById('12hourFormat').hidden = true; 
+        document.getElementById("toggleButton").innerHTML = "12 Hour";
+        toggle = 24;
+    };
+
+   
+}
